e7a074ad095889de5402c5be196937646f966dd8
1Niaz <niazbekturov353@gmail.com>
2024-10-28 10:04:52 +0500

    bugs in feedbacks

--- scripts/feedbacks.js
+++ scripts/feedbacks.js
@@ -1,14 +1,26 @@
 const ReadFeedbackBtns = document.querySelectorAll('.feedback__read')
 
 ReadFeedbackBtns.forEach(ReadFeedbackBtn => {
-    const Feedback = document.querySelector('.one__feedback')
-    const FeedbackBG = document.querySelector('.one__feedback__bg')
+    const Feedback = ReadFeedbackBtn.closest('.feedbacks__cards__item').querySelector('.one__feedback')
+    const FeedbackBG = Feedback.querySelector('.one__feedback__bg')
+    const FeedbackCross = Feedback.querySelector('.one__feedback__cross')
+
     ReadFeedbackBtn.addEventListener('click', () => {
         Feedback.classList.add('active__block')
+        document.body.style.overflow = 'hidden'
     })
+
+    FeedbackCross.addEventListener('click', () => {
+        Feedback.classList.remove('active__block')
+        document.body.style.overflow = ''
+    })
+
     FeedbackBG.addEventListener('click', () => {
         Feedback.classList.remove('active__block')
+        document.body.style.overflow = ''
     })
+    
+    
 })
 
 
